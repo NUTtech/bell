8f5e4428b71f14b366eb104f23097af89db05556
Andrey Litvinenko <andreylit@gmail.com>
2021-10-13 17:44:31 +0300

    issue #10 add example usage code

---
+++ example_test.go
@@ -0,0 +1,59 @@
+package bell_test
+
+import (
+	"fmt"
+	"github.com/nuttech/bell"
+	"sort"
+	"time"
+)
+
+type CustomStruct struct {
+	name string
+	param int32
+}
+
+func Example() {
+	event := "event_name"
+	event2 := "event_name_2"
+
+	// add listener on event event_name
+	bell.Listen(event, func(message bell.Message) {
+		// we extend CustomStruct in message.Value
+		customStruct := message.Value.(CustomStruct)
+		fmt.Println(customStruct)
+	})
+	// add listener on event event_name_2
+	bell.Listen(event2, func(message bell.Message) {
+
+	})
+
+	// get event list
+	list := bell.List()
+
+	// only for test
+	sort.Strings(list)
+	fmt.Println(list)
+
+	// remove listeners on event_name_2
+	bell.Remove(event2)
+
+	// get event list again
+	fmt.Println(bell.List())
+
+	// check if exists event_name_2 event in storage
+	fmt.Println(bell.Has(event2))
+
+	// call event event_name
+	_ = bell.Ring(event, CustomStruct{name: "testName", param: 12})
+
+	// ONLY FOR EXAMPLE
+	// add sleep because the event handler does not have time
+	// to be processed before the completion of the script execution
+	time.Sleep(time.Millisecond * 50)
+
+	// Output:
+	// [event_name event_name_2]
+	// [event_name]
+	// false
+	// {testName 12}
+}
